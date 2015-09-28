3f4cfd01a3901114817342ebc3c3dde5fe70f958
Ziad Saab <ziad.saab@gmail.com>
2015-09-28 13:03:16 -0400

    Create boilerplate.js

---
+++ boilerplate.js
@@ -0,0 +1,23 @@
+var mysql = require('mysql');
+var Promise = require('bluebird');
+Promise.promisifyAll(mysql);
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+
+var connection = mysql.createConnection({
+  host     : process.env.IP,
+  user     : process.env.C9_USER,
+  password : '',
+  database : 'addressbook'
+});
+
+connection.queryAsync("SELECT * FROM Account").then(
+    function(results) {
+    	var rows = results[0];
+    	// do something with rows...
+    }
+).finally(
+    function() {
+        connection.end();
+    }
+);
