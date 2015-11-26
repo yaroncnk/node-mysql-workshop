d308660e43d01912ef5ecbffbf502e9d4955d340
Ziad Saab <ziad.saab@gmail.com>
2015-11-26 07:08:18 -0500

    Update boilerplate.js

--- boilerplate.js
+++ boilerplate.js
@@ -1,8 +1,4 @@
 var mysql = require('mysql');
-var Promise = require('bluebird');
-Promise.promisifyAll(mysql);
-Promise.promisifyAll(require("mysql/lib/Connection").prototype);
-Promise.promisifyAll(require("mysql/lib/Pool").prototype);
 
 var connection = mysql.createConnection({
   host     : process.env.IP,
@@ -11,13 +7,18 @@ var connection = mysql.createConnection({
   database : 'addressbook'
 });
 
-connection.queryAsync("SELECT * FROM Account").then(
-    function(results) {
-    	var rows = results[0];
-    	// do something with rows...
-    }
-).finally(
-    function() {
-        connection.end();
-    }
-);
+connection.query("SELECT * FROM Account", function(err, rows, fields) {
+  // In this callback, rows will be all the rows of the query, in a regular array of regular objects
+  // fields is not used very often, but it will contain a listing of the columns with some metadata
+  
+  // Here is an example usage:
+  rows.forEach(function(row) {
+    console.log('#' + row.id + ': ' + row.email);
+  });
+  // This code will output lines like:
+  // #1: john@smith.com
+  // #2: abc@def.com
+  // #5: xx@yy.com
+  
+  // Note that IDs do not have to be contiguous. If we DELETE rows, there will be holes in the ID list. This is normal.
+});
