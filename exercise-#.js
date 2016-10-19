b3d7f54fa86650c29e6099c8681cc8b6237d55b0
Yaron Cohen <yaroncnk@gmail.com>
2016-10-19 18:39:35 +0000

    commit 2

--- exercise-#.js
+++ exercise-#.js
@@ -1,4 +1,33 @@
-//Ex. 1 - prep stage
+// //Ex. 1 - prep stage
+// var mysql = require('mysql');
+// var table = require('cli-table');
+
+// var connection = mysql.createConnection({
+//     host: process.env.IP,
+//     user: process.env.C9_USER,
+//     password: '',
+//     database: 'addressbook'
+// });
+
+// connection.query("SHOW DATABASES", function(err, rows, fields) {
+//     if (err) {
+//         return err;
+//     }
+//     else {
+//         var db = rows;
+//         var table1 = new Table();
+
+//         db.forEach(function(row){
+//           table1.push(row); 
+//         })
+
+
+//         console.log(table.toString());
+//     }
+//     connection.end();
+// });
+
+//ex. 2
 var mysql = require('mysql');
 var table = require('cli-table');
 
@@ -9,22 +38,18 @@ var connection = mysql.createConnection({
     database: 'addressbook'
 });
 
-connection.query("SHOW DATABASES", function(err, rows, fields) {
+connection.query("SELECT id, email FROM Account LIMIT 5", function(err, rows, fields) {
     if (err) {
         return err;
     }
     else {
-        var db = JSON.parse(JSON.stringify(rows));
-        var table1 = new Table();
         
-        db.forEach(function(row){
-           table1.push(row); 
-        })
-        
+        console.log(rows);
+        rows.forEach(function(row) {
+    
+                console.log('#' + row.id + ': ' + row.email);
+        });
 
-        console.log(table.toString());
     }
     connection.end();
 });
-
-//ex. 2
